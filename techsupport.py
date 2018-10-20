329d4d50b95806e7a7d5186f43e94f0fea7f4b04
Prav <larchpraveen@gmail.com>
2018-10-20 20:35:17 +0000

    Initial

---
+++ techsupport.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+import annealing_decryption
+import re
+import string
+
+key = string.ascii_uppercase
+ciphertype = "substitution"
+
+@app.route('/')
+def my_form():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	text = request.form['text']
+	userinput = text.upper()
+	regex = re.compile('[^A-Z]')
+	print(regex.sub('', userinput), "HE")
+	return (annealing_decryption.anneal(regex.sub('', userinput), key, ciphertype, "swap", ""))
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=8080)
+	
