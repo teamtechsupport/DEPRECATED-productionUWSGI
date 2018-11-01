42f3e6171813e7efdd84eb72ab3410c2b456c7ae
Praveen <larchpraveen22@outlook.com>
2018-11-01 22:03:18 +0000

    Update techsupport.py

--- techsupport.py
+++ techsupport.py
@@ -14,7 +14,6 @@ def my_form():
 
 @app.route('/', methods=['GET', 'POST'])
 def my_form_post():
-<<<<<<< HEAD
     selected = []
     if request.form.get('substition'):
         selected.append("substition")
@@ -27,22 +26,8 @@ def my_form_post():
     userinput = text.upper()
     regex = re.compile('[^A-Z]')
     print(regex.sub('', userinput), "HE")
-    return (annealing_decryption.anneal(regex.sub('', userinput), key, ciphertype, "swap", ""))
-=======
-        selected = []
-        if request.form.get('substition'):
-            selected.append("substition")
-        if request.form.get('vigenere'):
-            selected.append("vigenere")
-        if request.form.get('columntrans'):
-            selected.append('columntrans')
-        print(json.dumps(selected))
-	text = request.form['text']
-	userinput = text.upper()
-	regex = re.compile('[^A-Z]')
-	decoded = annealing_decryption.anneal(regex.sub('', userinput), key, ciphertype, "swap", "")
+    decoded = annealing_decryption.anneal(regex.sub('', userinput), key, ciphertype, "swap", "")
     return render_template("result.html", data = decoded)
->>>>>>> 94fbdbfcfcbf3c4e471a708e3063a17b233fb74b
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)	
